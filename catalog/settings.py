cd6debd3a3d2b2590b0418bf03f5a4ea603ba6ef
Zeydel <slavarut@gmail.com>
2019-01-16 16:36:49 +0700

    second commit

--- catalog/settings.py
+++ catalog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appcatalog',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'catalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mycatalog',
+        'USER': 'zeydel',
+        'PASSWORD': 'qaz123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -119,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- catalog/urls.py
+++ catalog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from appcatalog.views import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', hello)
 ]
+
